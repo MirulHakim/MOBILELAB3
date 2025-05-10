e3e39e4ce2876e718bc35e16e9a780a80e2ea5df
Amirul Hakimi <223734@student.upm.edu.my>
2025-05-11 03:57:56 +0800

    final

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,71 +1,69 @@
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:mobilelab3/screens/home_screen.dart';
+import 'package:get/get.dart';
+import 'package:mobilelab3/controller/bottom_nav_controller.dart';
+import 'package:mobilelab3/screens/home/home_screen.dart';
+import 'package:mobilelab3/screens/profile/profile.dart';
+import 'package:mobilelab3/screens/search/search_screen.dart';
+import 'package:mobilelab3/screens/ticket/ticket_screen.dart';
 
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
+class BottomNavBar extends StatelessWidget {
+  BottomNavBar({super.key});
 
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
+  // Dependency injection using GetX
+  final BottomNavController controller = Get.put(BottomNavController());
 
-class _BottomNavBarState extends State<BottomNavBar> {
   //list is iterated using index
   final appScreens = [
-    HomeScreen(),
-    Text("Search"),
-    Text("Tickets"),
-    Text("Profile"),
+    const HomeScreen(),
+    const SearchScreen(),
+    const TicketScreen(),
+    const ProfileScreen(),
   ];
-
-  //change our index for bottom nav bar
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: const Text("My Tickets"))),
-      body: Center(child: appScreens[_selectedIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex, //changes index of navigation bar index
-        onTap: _onItemTapped, //calls function to change index variable
-        selectedItemColor: Colors.blueGrey,
-        unselectedItemColor: const Color(0xFF526400),
-        showSelectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_home_add_regular),
+    print("My tab val ${controller.selectedIndex.value}");
+    return Obx(() {
+      return Scaffold(
+        // appBar: AppBar(
+        //   title: Center(child: const Text("My tickets"))
+        //   ),
+        body: appScreens[controller.selectedIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: controller.selectedIndex.value,
+          onTap: controller.onItemTapped,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: const Color(0xFF526400),
+          showSelectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_add_filled),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+              label: "Tickets",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
 
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
-            label: "Home",
-          ),
-        ],
-      ),
-    );
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+              label: "Profile",
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-import 'base/bottom_nav_bar.dart';
+import 'package:mobilelab3/base/bottom_nav_bar.dart';
+import 'package:mobilelab3/base/utils/app_routes.dart';
+import 'package:mobilelab3/screens/all_tickets.dart';
+import 'package:mobilelab3/screens/home/all_hotels.dart';
+import 'package:mobilelab3/screens/hotel_detail.dart';
+import 'package:mobilelab3/screens/ticket/ticket_screen.dart';
 
 void main() {
   runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
@@ -12,7 +18,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      home: BottomNavBar(),
+      debugShowCheckedModeBanner: false, 
+      routes: {
+        AppRoutes.homePage: (context) => BottomNavBar(),
+        AppRoutes.allTickets: (context) => const AllTickets(),
+        AppRoutes.ticketScreen: (context) => const TicketScreen(),
+        AppRoutes.allHotels: (context) => const AllHotels(),
+        AppRoutes.hotelDetail: (context) => const HotelDetail(),
+      },
     );
   }
 }
